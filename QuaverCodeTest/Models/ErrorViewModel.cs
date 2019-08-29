45438e999b18758d540a99e09ad12308b673f329
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-08-28 19:42:26 -0500

    initial file commit

---
+++ QuaverCodeTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuaverCodeTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
